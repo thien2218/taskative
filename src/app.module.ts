44684b93ca484608a97022863c33c93d65143a4a
thien2218 <thien1367526@gmail.com>
2024-02-02 11:24:35 +0700

    Configure database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: []
+   imports: [
+      ConfigModule.forRoot({
+         isGlobal: true,
+         envFilePath: `.env.${process.env.NODE_ENV}`
+      })
+   ],
+   controllers: [],
+   providers: []
 })
 export class AppModule {}
